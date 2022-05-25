94bc4663b354646272dbb10964f1ae1851bb6b40
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-05-26 02:26:06 +0600

    order post to database and quantity change added

--- index.js
+++ index.js
@@ -20,23 +20,43 @@ async function run() {
         await client.connect();
         console.log('db connected');
         const productCollection = client.db('Cakeries_bd').collection('products');
+        const orderCollection = client.db('Cakeries_bd').collection('orders');
 
+        // To load product in home page
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
-
+        // after clicking purchase btn route to shop and showing details
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
         })
+        // To post clients order in mongodb
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        }) 
 
-
-
+        //  update stock after any order 
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    stock: updatedProduct.stock
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
 
     }
     finally {
