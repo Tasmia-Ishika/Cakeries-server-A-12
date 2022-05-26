2d68e0fd91e45349185c1c9f1e9f0c0646bb940a
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-05-26 20:55:42 +0600

    identify admin from db

--- index.js
+++ index.js
@@ -122,6 +122,17 @@ async function run() {
             res.send(users);
         })
 
+        // identify admin from db
+        app.get('/admin/:email', async(req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+
+        })
+
+
+
         //making an admin setup
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
