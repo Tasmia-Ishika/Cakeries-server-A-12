f0ef22655991f1bd188edb20783aa250239b5bb6
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-05-26 20:47:10 +0600

    set up of limited admin

--- index.js
+++ index.js
@@ -123,14 +123,21 @@ async function run() {
         })
 
         //making an admin setup
-        app.put('/user/admin/:email', async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send( result );
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
         })
 
     }
